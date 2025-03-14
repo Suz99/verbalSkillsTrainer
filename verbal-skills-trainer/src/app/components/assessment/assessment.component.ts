80a004aba5e3d4fb62310123d39c0ea820ad9d8b
suz99 <suzanaisaac@gmail.com>
2025-03-14 22:00:46 +0530

    assessment: functionality - prompts user to add the presentation text and feedback will be produced
    Signed-off-by: suz99 <suzanaisaac@gmail.com>

--- verbal-skills-trainer/src/app/components/assessment/assessment.component.ts
+++ verbal-skills-trainer/src/app/components/assessment/assessment.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-
+import { OpenaiService } from '../../services/openai.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-assessment',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],  // Add FormsModule if using ngModel
   templateUrl: './assessment.component.html',
   styleUrl: './assessment.component.scss'
 })
 export class AssessmentComponent {
 
+  presentationText: string = '';
+  feedback: string = '';
+
+  constructor(private openAiService: OpenaiService) {}
+
+  assessPresentation() {
+    const prompt = `Analyze this presentation for structure, clarity, and persuasiveness: "${this.presentationText}"`;
+    this.openAiService.getChatResponse(prompt).subscribe(response => {
+      this.feedback = response.choices[0].message.content;
+    });
+  }
 }
