7ab983227e127e6584fecb9a71f93538f29975a3
suz99 <suzanaisaac@gmail.com>
2025-03-14 21:59:01 +0530

    app component: nav bar - created a nav bar for the application for chat, voice and assessment
    Signed-off-by: suz99 <suzanaisaac@gmail.com>

--- verbal-skills-trainer/src/app/app.component.ts
+++ verbal-skills-trainer/src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, RouterModule],  // Add RouterOutlet and FormsModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
